a121cf8fcfe8b131ea86f6241921a46d593ce455
Eider Prieto <prietoeider1@gmail.com>
2022-06-29 00:59:19 -0500

    MediaQueries Creation

--- script/js.js
+++ script/js.js
@@ -1,23 +1,39 @@
 
 const apiButton = document.getElementById('apiButton');
 
-let pokemons = [500];
-
-const callAPI =() =>{
+const objetos = function (numberOfpokes){
+    let  pokemons = {numberOfpokes};
     for(let i =1 ;i<501;i++){
         const link = `https://pokeapi.co/api/v2/pokemon/${i}`;
         fetch(link)
         .then(res =>res.json())
         .then(data =>{
-            pokemons[i-1] = data;
-
+            pokemons[i-1]   = data;
             let parragraf = document.getElementById('content');
-            parragraf.innerHTML = `<p>${data.name}</p>`;
+            parragraf.innerHTML += `<p>${data.name}</p>`;
         }); 
     }
-    console.log(pokemons);
+    return console.log(pokemons);
 };
 
 
+apiButton.addEventListener('click', () => {
+   let objects = objetos(50); 
 
-apiButton.addEventListener('click', callAPI);
+});
+
+// const objetos = () => {
+//     const link = 'https://pokeapi.co/api/v2/pokemon?limit=500&offset=0';
+//     fetch(link)
+//     .then(res =>res.json())
+//     .then(data =>{
+
+//         console.log(data);
+
+//         // let parragraf = document.getElementById('content');
+//         // parragraf.innerHTML = `<p>${data.name}</p>`;
+//     }); 
+
+// }   
+
+// apiButton.addEventListener('click', objetos);
