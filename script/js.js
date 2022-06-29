f1d3e6de5fa1fbc76bc89b2496bc0e91f57e0bae
Eider Prieto <prietoeider1@gmail.com>
2022-06-29 04:06:36 -0500

    adjustment in js

--- script/js.js
+++ script/js.js
@@ -3,12 +3,14 @@ const apiButton = document.getElementById('apiButton');
 
 const objetos = function (numberOfpokes){
     let  pokemons = {numberOfpokes};
+    let types;
     for(let i =1 ;i<=numberOfpokes;i++){
         const link = `https://pokeapi.co/api/v2/pokemon/${i}`;
         fetch(link)
         .then(res =>res.json())
         .then(data =>{
             pokemons[i-1]   = data;
+            // console.log(data.types.length)
             let parragraf = document.getElementById('content');
             parragraf.innerHTML += ` 
                 <div class="pokemon">
@@ -17,14 +19,12 @@ const objetos = function (numberOfpokes){
                     </div>
                     <h2 class="pokemon__numero">N#: ${data.order}</h2>
                     <div class="pokemon__descripcion grid">
-                
+                        <div class="pokemon__descripcion__type" style="border: 1px solid black;">${data.types[0].type.name}</div>                      
                     </div>
-            
-
                 </div>`;
         }); 
     }
-    return console.log(pokemons);
+    // return console.log(data.types.length);
 };
 
 
