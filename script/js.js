b34fce74b0171091375288a3a4677426e892187f
Eider Prieto <prietoeider1@gmail.com>
2022-06-29 18:35:50 -0500

    getting the list to abilities

--- script/js.js
+++ script/js.js
@@ -1,54 +1,72 @@
 
 const apiButton = document.getElementById('apiButton');
 
-const objetos = function (numberOfpokes){
+const objetos = async function (numberOfpokes){
     let  pokemons = {numberOfpokes};
-    let types;
     for(let i =1 ;i<=numberOfpokes;i++){
         const link = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        fetch(link)
-        .then(res =>res.json())
-        .then(data =>{
-            pokemons[i-1]   = data;
-            // console.log(data.types.length)
+        const rest = await fetch(link);
+        const json = await rest.json();
+        // .then(res =>res.json())
+        // .then(data =>{
+            pokemons[i-1]= json;
             let parragraf = document.getElementById('content');
+            // console.log(pokemons);
             parragraf.innerHTML += ` 
                 <div class="pokemon">
-                    <h1 class="pokemon__nombre">${data.name}</h1>
-                    <div class="pokemon__img" style="background-image:url('${data.sprites.front_default}');">
+                    <h1 class="pokemon__nombre">${json.name}</h1>
+                    <div class="pokemon__img" style="background-image:url('${json.sprites.front_default}')" 
+                    onclick="list('${json.name}','${json.sprites.front_default}','${json.abilities}','${json.stats}')">
                     </div>
-                    <h2 class="pokemon__numero">N#: ${data.order}</h2>
-                    <div class="pokemon__descripcion grid">
-                        <div class="pokemon__descripcion__type" style="border: 1px solid black;">${data.types[0].type.name}</div>                      
+                    <h2 class="pokemon__numero">N#: ${json.order}</h2>
+                    <div class="pokemon__descripcion grid" jsonName="${json.name} jsonImagen="${json.imagen}">
+                        <div class="pokemon__descripcion__type" style="border: 1px solid black;">${json.types[0].type.name}</div>
                     </div>
-                </div>`;
-        }); 
+                </div>               
+                
+                
+                `;
     }
-    // return console.log(data.types.length);
-};
+    // capturarDatos();
+    return pokemons;
+}
+objetos(50);
+// let documento = document.getElementsByClassName("pokemon");
 
 
-apiButton.addEventListener('click', () => {
-   let objects = objetos(500); 
+// console.log(typeof documento);
+function list(name,image,abilities,stats){
+    console.log(`this is the name: ${name}\nthis is the image:${image}\nthesee are the skills: ${abilities}\nthesee are the stats: ${stats}`)
+    // console.log('yupi')
+}
 
-});
-
-
-
-// const objetos = () => {
-//     const link = 'https://pokeapi.co/api/v2/pokemon?limit=500&offset=0';
-//     fetch(link)
-//     .then(res =>res.json())
-//     .then(data =>{
-
-//         console.log(data);
-
-//         // let parragraf = document.getElementById('content');
-//         // parragraf.innerHTML = `<p>${data.name}</p>`;
-//     }); 
-
+// for (let i = 0;i< documento.length ;i++){
+//     console.log('entro');
+//     documento[i].addEventListener('click',consolelog);
+//         // console.log(documento);
 // }   
 
-// apiButton.addEventListener('click', objetos);
 
 
+
+apiButton.addEventListener('click',function(){
+            
+
+});
+    
+    // variable.addEventListener('click',() => {
+        //     console.log(this.name)
+        // })
+
+        // document.querySelector('.pokemon__img').addEventListener('click',() => {
+        //     console.log(this.variable.name);
+        // })
+    
+// function capturarDatos(nombre){
+//     let name,imagen;
+//     let data = this.document.querySelector('.pokemon__descripcion');
+    
+//     name = data.getAttribute("jsonName");
+//     console.log(nombre);
+    
+// }
